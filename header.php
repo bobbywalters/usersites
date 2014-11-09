5751cf6fa4ee9c7cb505d04966a59b27a91e5e64
Bobby Walters <bobbymwalters@gmail.com>
2014-11-09 18:01:12 -0500

    No need for pingback info

--- header.php
+++ header.php
@@ -14,8 +14,6 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url'); ?>">
-		<link href="<?php echo get_stylesheet_directory_uri() ?>/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
